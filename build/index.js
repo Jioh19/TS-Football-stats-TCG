44810eaa3f4a6c375aec7bfed43a80a9b85ffb7a
Jioh19 <jioh1982@gmail.com>
2023-10-08 16:10:09 -0300

    init

--- build/index.js
+++ build/index.js
@@ -1,11 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const CsvFileReader_1 = require("./CsvFileReader");
+const MatchReader_1 = require("./MatchReader");
 const MatchResult_1 = require("./MatchResult");
-const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+const reader = new MatchReader_1.MatchReader('football.csv');
 reader.read();
-const dateOfFirstMatch = reader.data[0];
-console.log(reader.data[0]);
 let manUnitedWins = 0;
 for (let match of reader.data) {
     if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new CsvFileReader('football.csv');
+const reader = new MatchReader('football.csv');
 reader.read();
-
-const dateOfFirstMatch = reader.data[0];
-console.log(reader.data[0]);
 
 let manUnitedWins = 0;
 
